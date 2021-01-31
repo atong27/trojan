c84412c890fbf2c8653045e9041b4d006d051f1f
atong27 <ayrtonilagan@gmail.com>
2021-01-31 09:23:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ REMOVE=0
 
 UPDATE=0
 
-DOWNLAOD_URL="https://github.com/Jrohy/trojan/releases/download/"
+DOWNLAOD_URL="https://github.com/atong27/trojan/releases/download/"
 
-VERSION_CHECK="https://api.github.com/repos/Jrohy/trojan/releases/latest"
+VERSION_CHECK="https://api.github.com/repos/atong27/trojan/releases/latest"
 
 SERVICE_URL="https://raw.githubusercontent.com/Jrohy/trojan/master/asset/trojan-web.service"
 
