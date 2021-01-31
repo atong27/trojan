84d2b7081d2cf5c6d0cbc0a14a8e04aa0c196b2d
atong27 <ayrtonilagan@gmail.com>
2021-01-31 08:54:00 +0800

    translated to english

--- trojan/web.go
+++ trojan/web.go
@@ -10,8 +10,8 @@ import (
 // WebMenu web管理菜单
 func WebMenu() {
 	fmt.Println()
-	menu := []string{"重置web管理员密码", "修改显示的域名(非申请证书)"}
-	switch util.LoopInput("请选择: ", menu, true) {
+	menu := []string{"Reset the web administrator password", "Modify the displayed domain name (not to apply for a certificate)"}
+	switch util.LoopInput("Please choose: ", menu, true) {
 	case 1:
 		ResetAdminPass()
 	case 2:
@@ -21,14 +21,14 @@ func WebMenu() {
 
 // ResetAdminPass 重置管理员密码
 func ResetAdminPass() {
-	inputPass := util.Input("请输入admin用户密码: ", "")
+	inputPass := util.Input("Please enter the admin user password: ", "")
 	if inputPass == "" {
-		fmt.Println("撤销更改!")
+		fmt.Println("Undo changes!")
 	} else {
 		encryPass := sha256.Sum224([]byte(inputPass))
 		err := core.SetValue("admin_pass", fmt.Sprintf("%x", encryPass))
 		if err == nil {
-			fmt.Println(util.Green("重置admin密码成功!"))
+			fmt.Println(util.Green("Successfully reset admin password!"))
 		} else {
 			fmt.Println(err)
 		}
@@ -38,14 +38,14 @@ func ResetAdminPass() {
 // SetDomain 设置显示的域名
 func SetDomain(domain string) {
 	if domain == "" {
-		domain = util.Input("请输入要显示的域名地址: ", "")
+		domain = util.Input("Please enter the domain name address to be displayed: ", "")
 	}
 	if domain == "" {
-		fmt.Println("撤销更改!")
+		fmt.Println("Undo changes!")
 	} else {
 		core.WriteDomain(domain)
 		Restart()
-		fmt.Println("修改domain成功!")
+		fmt.Println("Modified domain successfully!")
 	}
 }
 
